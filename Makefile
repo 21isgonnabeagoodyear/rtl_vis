f8d271b0ed8d6d410de0d2061193c2984bf77a37
Thomas V <imadoofus123@yahoo.com>
2014-10-03 16:31:28 -0500

    make plotfulldft now does frequency scanning

--- Makefile
+++ Makefile
@@ -1,9 +1,17 @@
 all: rtl_t1
 rtl_t1: rtl_t1.c
-	gcc rtl_t1.c convenience/convenience.c -lrtlsdr -lfftw3 -o rtl_t1
+	gcc $(CFLAGS) rtl_t1.c convenience/convenience.c -lrtlsdr -lfftw3 -lm -g -o rtl_t1
+clean:
+	rm rtl_t1
 leakcheck: rtl_t1
 	valgrind --leak-check=full ./rtl_t1
 run: rtl_t1
 	./rtl_t1
 plotdft: run
 	gnuplot -persist -e "set autoscale fix;plot \"testdft.dat\" using 1:2 with lines" 
+plotfulldft: rtl_t1.c
+	#I don't know how to use variables
+	gcc -DBIGDFT rtl_t1.c convenience/convenience.c -lrtlsdr -lfftw3 -lm -g -o rtl_t1
+	./rtl_t1
+	gnuplot -persist -e "set autoscale fix;plot \"bigdft.dat\" using 1:2 with lines" 
+	
--- rtl_t1.c
+++ rtl_t1.c
@@ -3,27 +3,33 @@
 #include "convenience/convenience.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <float.h>
 #include "fftw3.h"
+
+#define SAMPLERATE ((2048000))
+#define BUFLEN (((1<<16)/64))
 
 
 int main(){
+	printf("samplerate: %d\nbuflen:%d\n", SAMPLERATE, BUFLEN);
 	rtlsdr_dev_t *dev = NULL;
 	int devindex = verbose_device_search("0");
 
 	int r = rtlsdr_open(&dev, devindex);
 	if(r){printf("err %d opening device\n", r);}
-	verbose_set_sample_rate(dev, 2048000);
+	verbose_set_sample_rate(dev, SAMPLERATE);
 	verbose_set_frequency(dev, 90000000);//357000);
-	verbose_gain_set(dev, 207);
+	//verbose_gain_set(dev, 496);
+	verbose_gain_set(dev, 200);
 	//verbose_auto_gain(dev);
 
 	verbose_reset_buffer(dev);
-	char buf[(1<<16)*2] = {0};
+	char *buf = calloc(BUFLEN,1);
 	//char *buf = malloc((1<<16)*8);//[(1<<16)*8] = {0};
 	int howmany = 0;
-	rtlsdr_read_sync(dev, buf, sizeof(buf), &howmany);
-	printf("read %d of %d\n", howmany, sizeof(buf));
-	rtlsdr_close(dev);
+	rtlsdr_read_sync(dev, buf, BUFLEN, &howmany);
+	printf("read %d of %d\n", howmany, BUFLEN);
 
 	FILE *testfile = fopen("test.dat", "w");
 	//fprintf(testfile, "[\n");
@@ -36,9 +42,9 @@ int main(){
 
 
 
-	double inbuf[sizeof(buf)/2] = {0};
-	fftw_complex outbuf[sizeof(buf)/2] = {0};
-	fftw_plan plan = fftw_plan_dft_r2c_1d(sizeof(buf)/2, inbuf, outbuf, FFTW_ESTIMATE);
+	double *inbuf = calloc(BUFLEN/2, sizeof(double));
+	fftw_complex *outbuf = calloc(BUFLEN/2,  sizeof(fftw_complex));
+	fftw_plan plan = fftw_plan_dft_r2c_1d(BUFLEN/2, inbuf, outbuf, FFTW_ESTIMATE);
 
 
 
@@ -49,9 +55,56 @@ int main(){
 	fftw_execute(plan);
 	testfile = fopen("testdft.dat", "w");
 	for(i=1/*ignore first component (dc?)*/;i<howmany/2/2;i++){
-		fprintf(testfile, "%d %f\n", i, outbuf[i][0]);
+		fprintf(testfile, "%d %f\n", i, log10(abs(outbuf[i][0]))-log10(DBL_MAX));//this math is probably/definitely all wrong
 	}
 	fclose(testfile);
 	//free(buf);
+
+
+
+
+
+
+
+
+
+
+#ifdef BIGDFT
+	testfile = fopen("bigdft.dat", "w");
+	int freq;
+	//for(freq=2000000;freq<1000000000;freq += 2048000){
+	//for(freq=80000000;freq<100000000;freq += SAMPLERATE/*BUFLEN/2*/){
+	for(freq=40000000;freq<1000000000;freq += SAMPLERATE/*BUFLEN/2*/){
+		verbose_set_frequency(dev, freq);//357000);
+		verbose_reset_buffer(dev);
+		rtlsdr_read_sync(dev, buf, BUFLEN, &howmany);
+		for(i=0;i<howmany/2;i++){
+			inbuf[i] = (double)((int)((unsigned short*)buf)[i] - 0xffff/2);
+		}
+		fftw_execute(plan);
+		for(i=1;i<howmany/2/2;i+= 1){//50){
+			//fprintf(testfile, "%d %f\n", i*(SAMPLERATE/(howmany/2/2))+freq, (outbuf[i][0]));//this math is probably/definitely all wrong
+			fprintf(testfile, "%d %f\n", i*(SAMPLERATE/(howmany/2/2))+freq, log10(abs(outbuf[i][0]))-log10(DBL_MAX));//this math is probably/definitely all wrong
+		}
+		printf("%d\n", freq);
+	}
+	fclose(testfile);
+#endif
+
+
+
+
+
+
+
+
+
+
+
+	rtlsdr_close(dev);
+
+
+
+
 	return 0;
 }
